ec095ff87d9329e9508cbe2fa0d8cbda622e1bf9
Julie Broderick <broderick.julie@gmail.com>
2019-03-21 19:34:14 -0500

    added accordian component

---
+++ src/Accordian.js
@@ -0,0 +1,47 @@
+import React, { Component } from 'react';
+
+export default class Accordian extends Component {
+    static defaultProps = {
+        sections: {
+            title: '',
+            content: ''
+        }
+    }
+
+    state = {
+        tabIndex: null
+    }
+
+    handleClick(index) {        
+        this.setState({
+            tabIndex: index
+        })
+    }
+    
+    renderButtons = () => {
+        return this.props.sections.map((section, index) => 
+            <li key={index}>
+                <button onClick={() => this.handleClick(index)}>
+                    {section.title}
+                </button>
+                <p>
+                    { (this.state.tabIndex === index) && section.content}
+                </p>
+            </li>)
+    }
+
+    render() {        
+        return (
+            <div>
+                <ul>
+                    {this.renderButtons()}
+                </ul>
+            </div>
+        )
+    }
+}
+
+
+
+
+
--- src/App.js
+++ src/App.js
@@ -1,11 +1,27 @@
 import React, { Component } from 'react';
+import Accordian from './Accordian';
 import './App.css';
+
+const sections = [
+  {
+    title: 'Section 1',
+    content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit.',
+  },
+  {
+    title: 'Section 2',
+    content: 'Cupiditate tenetur aliquam necessitatibus id distinctio quas nihil ipsam nisi modi!',
+  },
+  {
+    title: 'Section 3',
+    content: 'Animi amet cumque sint cupiditate officia ab voluptatibus libero optio et?',
+  },
+]
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-       
+        <Accordian sections={sections} />
       </div>
     );
   }
